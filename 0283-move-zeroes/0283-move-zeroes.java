e51d5b0935ae206533aa250c35dbb12b8570b0b3
Muneera Parveen H <muneeraparveen2k5@gmail.com>
2026-07-27 14:14:37 +0530

    Time: 1 ms (99.87%), Space: 47.2 MB (97.95%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int [] result=new int[n];
+        int j=0;
+        for(int i=0;i<n;i++) {
+            if(nums[i]!=0){
+                result[j]=nums[i];
+                j++;
+            }
+        }
+         for(int i = 0; i < n; i++) {
+            nums[i] = result[i];
+        }
+    }
+}
