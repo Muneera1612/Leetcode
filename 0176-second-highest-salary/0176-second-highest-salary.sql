be26c54b0fa603bcb69b9223f4d9c6264b54e885
Muneera Parveen H <muneeraparveen2k5@gmail.com>
2026-07-29 09:41:32 +0530

    Time: 289 ms (50.75%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,9 @@
+SELECT
+    (
+        SELECT MAX(salary)
+        FROM Employee
+        WHERE salary < (
+            SELECT MAX(salary)
+            FROM Employee
+        )
+    ) AS SecondHighestSalary;
